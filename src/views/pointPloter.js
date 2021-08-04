e46e185db1c1a5e6112d1fd61829161e2aa3e4f9
weidehai <243395655@qq.com>
2021-08-04 21:17:53 +0800

    绘制部分重构

--- src/views/pointPloter.js
+++ src/views/pointPloter.js
@@ -1,11 +1,8 @@
 import { degToarc, reliableFloatAdd, isNull, isArray } from "ifuncs";
 
 export const PonitPloter = (function() {
-  let x = null;
-  let y = 0;
-  let degx = 0;
   let ratio = 0;
-  let _options;  //sava options
+  let _options; //sava options
   let cartesianContext = {
     x: null,
     y: null,
@@ -13,7 +10,9 @@ export const PonitPloter = (function() {
     cartesianStep: 0.01
   };
   let polarContext = {
-    degx: 0,
+    degVariable: 0,
+    x:0,
+    y:0,
     polarRange: [0, 360],
     polarStep: 1
   };
@@ -24,18 +23,9 @@ export const PonitPloter = (function() {
     _y: null //previous ponit
   };
   function PonitPloter(options) {
-    this.x = null;
-    this.y = 0;
-    this._x = 0;
-    this.realx = 0;
-    this.realy = 0;
-    this._realx = null;
-    this._realy = null;
-    this.stopPlot = false;
-    this.pointCount = 0;
     this.canvas = options.canvas;
     _options = options;
-    init(this,options);
+    init(this, options);
   }
   function init(pp, options) {
     ratio = (pp.canvas.cellSize * pp.canvas.per) / pp.canvas.ratio;
@@ -62,54 +52,63 @@ export const PonitPloter = (function() {
       if (options.polarStep)
         polarContext.polarStep = parseInt(options.polarStep);
     }
+    function initCalculateCanvasPoint(){
+      calculateCanvasPoint = {
+        x: 0,
+        y: 0,
+        _x: null,
+        _y: null
+      };
+    }
     initCartesianContext();
     initPolarContext();
+    initCalculateCanvasPoint()
   }
 
   PonitPloter.prototype.setExp = function(expression) {
     this.expression = expression;
   };
   PonitPloter.prototype.setPolarRange = function(range) {
-    this.polarRange = range;
+    _options.polarRange = range;
   };
   PonitPloter.prototype.setCartesianRange = function(range) {
-    this.cartesianRange = range;
+    _options.cartesianRange = range;
   };
   PonitPloter.prototype.polar = function() {
     let pp = this;
-    this._x = parseInt(this.polarRange[0]);
-    console.log(this.polarRange);
-    if (!this.x) this.x = degToarc(this._x);
-    for (; this._x <= parseInt(this.polarRange[1]); ) {
-      console.log(1111);
+    ratio = (pp.canvas.cellSize * pp.canvas.per) / pp.canvas.ratio;
+    let expression = this.expression
+      .replace("x", "polarContext.x")
+      .replace("y", "polarContext.y");
+    polarContext.degVariable = parseFloat(polarContext.polarRange[0]);
+    polarContext.x = degToarc(polarContext.degVariable);
+    for (
+      ;
+      polarContext.degVariable <= parseFloat(polarContext.polarRange[1]);
+
+    ) {
       try {
-        eval(this.expression);
+        eval(expression);
       } catch (e) {
         throw e;
       }
-      this.pointCount++;
-      this.realx =
+      calculateCanvasPoint.x =
         this.canvas.originPointX +
-        this.y * Math.cos(degToarc(this._x)) * this.zoom;
-      this.realy =
+        polarContext.y *
+          Math.cos(polarContext.x) *
+          ratio;
+      calculateCanvasPoint.y =
         this.canvas.originPointY -
-        this.y * Math.sin(degToarc(this._x)) * this.zoom;
-      console.log(this.realy, this.realx);
-      if (isInlayout(this.realx, this.realy, pp) && !this.stopPlot) {
-        if (!this._realx || !this._realy) {
-          (this._realx = this.realx), (this._realy = this.realy);
-        } else {
-          let point1 = [this._realx, this._realy];
-          let point2 = [this.realx, this.realy];
-          this.canvas.pen.drawLine(point1, point2, "red");
-          (this._realx = this.realx), (this._realy = this.realy);
-        }
+        polarContext.y *
+          Math.sin(polarContext.x) *
+          ratio;
+      if (isInlayout(calculateCanvasPoint.x, calculateCanvasPoint.y, pp)) {
+        drawIfInNeed(pp)
       } else {
-        (this._realx = null), (this._realy = null);
+        (calculateCanvasPoint._x = null), (calculateCanvasPoint._y = null);
       }
-      if (this.stopPlot) break;
-      this._x = reliableFloatAdd(this._x, this.polarStep);
-      this.x = degToarc(this._x);
+      polarContext.degVariable = reliableFloatAdd(polarContext.degVariable, polarContext.polarStep);
+      polarContext.x = degToarc(polarContext.degVariable);
     }
   };
   PonitPloter.prototype.cartesian = function() {
@@ -124,31 +123,38 @@ export const PonitPloter = (function() {
       } catch (e) {
         throw e;
       }
-      calculateCanvasPoint.x = this.canvas.originPointX + cartesianContext.x * ratio;
-      calculateCanvasPoint.y = this.canvas.originPointY - cartesianContext.y * ratio;
+      calculateCanvasPoint.x =
+        this.canvas.originPointX + cartesianContext.x * ratio;
+      calculateCanvasPoint.y =
+        this.canvas.originPointY - cartesianContext.y * ratio;
       if (isInlayout(calculateCanvasPoint.x, calculateCanvasPoint.y, pp)) {
-        if (!calculateCanvasPoint._x || !calculateCanvasPoint._y) {
-          (calculateCanvasPoint._x = calculateCanvasPoint.x),
-            (calculateCanvasPoint._y = calculateCanvasPoint.y);
-        } else {
-          let point1 = [calculateCanvasPoint._x, calculateCanvasPoint._y];
-          let point2 = [calculateCanvasPoint.x, calculateCanvasPoint.y];
-          this.canvas.pen.drawLine(point1, point2, "red");
-          (calculateCanvasPoint._x = calculateCanvasPoint.x),
-            (calculateCanvasPoint._y = calculateCanvasPoint.y);
-        }
+        drawIfInNeed(pp)
       } else {
-        (calculateCanvasPoint._x = null),
-            (calculateCanvasPoint._y = null);
+        (calculateCanvasPoint._x = null), (calculateCanvasPoint._y = null);
       }
-      cartesianContext.x = reliableFloatAdd(cartesianContext.x, cartesianContext.cartesianStep * this.canvas.ratio);
+      cartesianContext.x = reliableFloatAdd(
+        cartesianContext.x,
+        cartesianContext.cartesianStep * this.canvas.ratio
+      );
     }
   };
+  function drawIfInNeed(pp){
+    if (!calculateCanvasPoint._x || !calculateCanvasPoint._y) {
+      (calculateCanvasPoint._x = calculateCanvasPoint.x),
+        (calculateCanvasPoint._y = calculateCanvasPoint.y);
+    } else {
+      let point1 = [calculateCanvasPoint._x, calculateCanvasPoint._y];
+      let point2 = [calculateCanvasPoint.x, calculateCanvasPoint.y];
+      pp.canvas.pen.drawLine(point1, point2, "red");
+      (calculateCanvasPoint._x = calculateCanvasPoint.x),
+        (calculateCanvasPoint._y = calculateCanvasPoint.y);
+    }
+  }
   function isInlayout(x, y, pp) {
     return x >= 0 && x <= pp.canvas.width && y >= 0 && y <= pp.canvas.height;
   }
   PonitPloter.prototype.refresh = function() {
-    init(this,_options)
+    init(this, _options);
   };
   return PonitPloter;
 })();
